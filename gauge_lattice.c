256ec7e8806b7ec063698e49907f2d9241a87b1e
Johann Ostmeyer <ostmeyer@hiskp.uni-bonn.de>
2024-02-13 12:24:07 +0000

    added lattice

--- gauge_lattice.c
+++ gauge_lattice.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <complex.h>
 
+#include "gauge_flags.h"
+
 #include "gauge_lattice.h"
 
-unsigned *construct_lattice(char *geometry, FILE *input, unsigned *ns, unsigned *nn, unsigned *ng, double **s){
+unsigned *construct_lattice(FILE *input, unsigned *ns, unsigned *nn, gauge_flags *mode){
 	unsigned *nnt;
+
+	char geometry[100];
+	fscanf(input, "geometry = %s\n", geometry);
+
 	if(!strcmp(geometry, "cubic")){
-		nnt = construct_cubic(input, ns, nn, ng, s);
-	// Geometries below are not tested!
+		nnt = construct_cubic(input, ns, nn, mode);
+	// Geometries below are not adjusted and will fail!
 	//}else if(!strcmp(geometry, "rectangular")){
 	//	nnt = construct_rectangular(input, ns, nn, s);
 	//}else if(!strcmp(geometry, "triangular")){
@@ -20,21 +27,19 @@ unsigned *construct_lattice(char *geometry, FILE *input, unsigned *ns, unsigned 
 		printf("The geometry \"%s\" is not known!\n", geometry);
 		exit(0);
 	}
-	fclose(input);
+
 	return nnt;
 }
 
-unsigned *construct_cubic(FILE *input, unsigned *ns, unsigned *nn, unsigned *ng, double **s){
-	unsigned l, dim;
+unsigned *construct_cubic(FILE *input, unsigned *ns, unsigned *nn, gauge_flags *mode){
+	unsigned l, dim, d;
 	unsigned *N, *nnt;
-	char boundaries[100];
 
-	fscanf(input, "dimension=%u\n", &dim);
-	fscanf(input, "length=%u\n", &l);
-	fscanf(input, "generators=%u\n", ng);
+	fscanf(input, "dimension = %u\n", &dim);
+	fscanf(input, "length = %u\n", &l);
 
-	*nl = l;
-	*ndim = dim;
+	mode->length_cube = l;
+	mode->space_dim = dim;
 
 	*nn = 2*dim;
 	N = malloc(dim * sizeof(unsigned));
@@ -46,25 +51,24 @@ unsigned *construct_cubic(FILE *input, unsigned *ns, unsigned *nn, unsigned *ng,
 
 	const unsigned size = *ns;
 	const unsigned neighbours = *nn, links = neighbours/2;
-	const unsigned generators = *ng;
+	const unsigned generators = mode->num_gen;
 
 	nnt = malloc(2 * neighbours * size * sizeof(unsigned));
-	*s = malloc(generators * links * size * sizeof(double));
 
 	for(unsigned i = 0; i < size; i++){
-		for(unsigned d = 0; d < dim; d++){
+		for(d = 0; d < dim; d++){
 			const unsigned id = (i/N[d]) % l;
 
 			// nearest sites
-			shift = i*neighbours;
-			nnt[shift + 2*d]     = generators * (i + ((id+1  )%l - id)*N[d]); // right, top,...
-			nnt[shift + 2*d + 1] = generators * (i + ((id+l-1)%l - id)*N[d]); // left, bottom,...
-			const unsigned im = nnt[shift + 2*d + 1];
+			unsigned shift = i*neighbours;
+			nnt[shift + d]         = i + ((id+1  )%l - id)*N[d]; // right, top,...
+			nnt[shift + d + links] = i + ((id+l-1)%l - id)*N[d]; // left, bottom,...
+			const unsigned im = nnt[shift + d + links];
 
 			// nearest links
 			shift = (size + i)*neighbours;
-			nnt[shift + 2*d]     = generators * (d + links*i); // right, top,...
-			nnt[shift + 2*d + 1] = generators * d + links*im; // left, bottom,...
+			nnt[shift + d]         = generators * (d + links*i); // right, top,...
+			nnt[shift + d + links] = generators * (d + links*im); // left, bottom,...
 		}
 	}
 
